273ae9afd13199aaf22cb3710737b5c6790a7ec5
王紫纯 <2391606500@qq.com>
2021-07-27 17:04:29 +0800

    第二次提交做到了详情页

--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
+import router from './router'
 
 Vue.config.productionTip = false
-
+    // 创建事件总线
+Vue.prototype.$bus = new Vue()
 new Vue({
-  render: h => h(App),
+    render: h => h(App),
+    router
 }).$mount('#app')
